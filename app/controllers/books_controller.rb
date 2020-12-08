66689c8309930316ff9a7ee695c4d65e974c0e46
GitHubのユーザ名 <GitHub>
2020-12-08 12:58:47 +0000

    [Fix]3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,41 @@
 class BooksController < ApplicationController
   def homes
   end
-  
+
   def index
       @book = Book.new
+      @books = Book.all
   end
 
-  def show
+  def showgit 
+     @book = Book.find(params[:id])
   end
-  
+
   def create
-    blog = Blog.new(blog_params)
-    blog.save
-    redirect_to blogs_path
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(blog_params)
+    redirect_to book_path(blog)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+    private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
