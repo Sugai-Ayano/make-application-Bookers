c13f99087ae349f635137076ed1b6ae3d4e1ac3b
GitHubのユーザ名 <GitHub>
2020-12-08 07:35:04 +0000

    [Fix]3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,18 @@
 class BooksController < ApplicationController
+  def homes
+  end
+  
   def index
+      @book = Book.new
   end
 
   def show
   end
-
-  def new
+  
+  def create
+    blog = Blog.new(blog_params)
+    blog.save
+    redirect_to blogs_path
   end
 
   def edit
